b871d4018923a7a113abc00b02b4fcf70de94a73
Vishant93 <vishantk@gmail.com>
2020-08-28 18:32:40 -0700

    Skeleton

--- src/main/java/com/vishant/sandwichtechnologies/DogRegistrationApplication.java
+++ src/main/java/com/vishant/sandwichtechnologies/DogRegistrationApplication.java
@@ -6,23 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DogRegistrationApplication implements CommandLineRunner {
-	@Autowired
-	DogRepository repository;
+public class DogRegistrationApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DogRegistrationApplication.class, args);
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		// put down the dogs
-		repository.deleteAll();
-
-	    // save a couple of dogs
-	    repository.save(new Dog("Ruffles", 3, "Labrador Retriever"));
-	    repository.save(new Dog("Pinto", 5, "Jack Russell Terrier"));
-	    repository.save(new Dog("Coco", 9, "Maltipoo"));
 	}
 
 }
